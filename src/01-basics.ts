b1e598a4aea8fd0aebe24439fc3dc8a3d6af7f7e
serenebabu2022 <serenebabu22@gmail.com>
2024-01-18 16:08:00 +0000

    Completed type safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -51,9 +51,15 @@ const friend1:Friend = {
       extension: 125,
     },
   };
-  const colleagues = {
+  interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+  }
+  export const colleagues: ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
   
   console.log(colleagues.current[0]);
+
+ 
