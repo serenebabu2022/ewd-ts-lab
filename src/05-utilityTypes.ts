708be2cf0f7bb4ee0aaae1ffdca9e1337dbde0bd
serenebabu2022 <serenebabu22@gmail.com>
2024-01-19 20:54:57 +0000

    Define types using Utility types.

---
+++ src/05-utilityTypes.ts
@@ -0,0 +1,63 @@
+import { friends, colleagues } from "./01-basics";
+import { Friend, Colleague, SecureFriendContact, FriendPartial, EventPass, FriendAndColleague } from "./myTypes";
+
+function updateFriend(friend: Friend, updates: FriendPartial ) : Friend {
+  return { ...friend, ...updates}
+}
+
+console.log(updateFriend(friends[0], {
+  phone: '08712345',
+  dob: new Date("1998-10-22")
+}))
+
+function secureFindFriends(
+    friends: Friend[],
+    criteria: (f: Friend) => boolean
+  ): SecureFriendContact[] {
+    const matches = friends.filter(criteria);
+    return matches.map((f) => {
+      const secure: SecureFriendContact = {
+        name: f.name,
+        phone: f.phone,
+      };
+      return secure;
+    });
+  }
+  let result = secureFindFriends(
+      friends,
+      (f: Friend) => f.age < 30
+  )
+  console.log(result)
+
+  function generateEventPass(colleague: Colleague): EventPass {
+    const passCode = Math.round(Math.random() * (1000 - 1) + 1);
+    return {
+      name: colleague.name,
+      department: colleague.department,
+      passCode: passCode,
+    };
+  }
+  console.log(generateEventPass(colleagues.current[0]));
+
+  function intersection(
+    friends: Friend[],
+    colleagues: Colleague[]
+  ): FriendAndColleague[]{
+    
+    let result:FriendAndColleague[]=[]
+    friends.reduce((res:FriendAndColleague[], friend) => {
+      const colleague = colleagues.find((col) => col.name === friend.name);
+      console.log("colleague",colleague)
+      if (colleague) {
+        // Colleague is also a Friend
+        res.push({name:colleague.name,
+                age:friend.age,
+                contact:colleague.contact})
+      }
+      console.log("res value",res)
+      return res;
+    }, result);
+    return result;
+  }
+  
+  console.log(intersection(friends, colleagues.current));
--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -44,3 +44,20 @@ export type BuddyList = {
   administrator: Administrator;
   members: Buddy[];
 };
+
+export type FriendPartial = Partial<Friend>
+// Type for gaining access to an event, e.g. concert.
+export type EventPass = Omit<Colleague, "contact"> & {
+  passCode : number;
+}
+// Immutable person type, based on Friend type.
+export type SecureFriendContact = Readonly<Pick<Friend,"name" | "phone" > >
+
+export type FriendAndColleague={
+  name:string;
+  age:number;
+  contact:{
+    email: string;
+    extension: number
+  } 
+}
